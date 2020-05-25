daec3129e0df3df37d35067a10592ee5c78737f2
jiangweixian <jiangweixian1994@gmail.com>
2020-05-25 18:37:20 +0800

    ⚡️ export

--- components/index.tsx
+++ components/index.tsx
@@ -3,5 +3,7 @@ import Sun from './Sun'
 import StarRings from './StarRings'
 import Snow from './Snow'
 import Meteor from './Meteor'
+import Cloudy from './Cloudy'
+import Wind from './Wind'
 
-export { Rain, Sun, StarRings, Snow, Meteor }
+export { Rain, Sun, StarRings, Snow, Meteor, Cloudy, Wind }
