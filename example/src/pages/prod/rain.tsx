b8ef595fc1516ed675844a4eb3382b4a30038321
jiangweixian <jiangweixian1994@gmail.com>
2020-05-25 15:44:50 +0800

    ➕ (example): add threejs weather

---
+++ example/src/pages/prod/rain.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Canvas, extend } from 'react-three-fiber'
+import * as meshline from 'threejs-meshline'
+
+import { Controls } from '@/components/Controls'
+import { Rain } from 'threejs-weather'
+
+extend(meshline)
+
+const RainPage = () => {
+  return (
+    <Canvas
+      pixelRatio={window.devicePixelRatio}
+      style={{ backgroundColor: '#1677b3' }} // rain: #1677b3; sun: #faf4e8
+    >
+      <Controls enableDamping={true} rotateSpeed={0.3} dampingFactor={1} />
+      <fog attach="fog" args={[0xffffff, 100, 100]} />
+      <Rain />
+      {/* <Wind count={10} /> */}
+    </Canvas>
+  )
+}
+
+export default RainPage
