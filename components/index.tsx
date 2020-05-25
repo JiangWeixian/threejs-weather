12e94eff6fb467b7c110d2d396473c13335c1cdb
jiangweixian <jiangweixian1994@gmail.com>
2020-05-25 19:19:48 +0800

    🚚 (meteors): meteor -> meteors

--- components/index.tsx
+++ components/index.tsx
@@ -2,8 +2,8 @@ import Rain from './Rain'
 import Sun from './Sun'
 import StarRings from './StarRings'
 import Snow from './Snow'
-import Meteor from './Meteor'
+import Meteors from './Meteors'
 import Cloudy from './Cloudy'
 import Wind from './Wind'
 
-export { Rain, Sun, StarRings, Snow, Meteor, Cloudy, Wind }
+export { Rain, Sun, StarRings, Snow, Meteors, Cloudy, Wind }
