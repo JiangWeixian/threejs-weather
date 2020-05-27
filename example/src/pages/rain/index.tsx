bafb06f5d66e1fdd0ab08933f0d8f557f21adfe7
jiangweixian <jiangweixian1994@gmail.com>
2020-05-27 21:49:21 +0800

    🚧 (slash): single one

--- example/src/pages/rain/index.tsx
+++ example/src/pages/rain/index.tsx
@@ -7,6 +7,7 @@ import { Text } from '@/components/Text'
 import Rain from '../../../../components/rain'
 import { useRef } from 'react'
 import { Mesh, Geometry, Object3D, Vector3 } from 'three'
+import { useMemo } from 'react'
 
 extend(meshline)
 
@@ -34,6 +35,58 @@ const Inter = () => {
   )
 }
 
+const length = 0.1
+
+const Slash = ({ vertices }) => {
+  const mat = useRef<any>()
+  useFrame(() => {
+    if (!mat.current) {
+      return
+    }
+    mat.current.uniforms.dashOffset.value += 0.01
+    mat.current.opacity -= 0.001
+  })
+  return (
+    <mesh>
+      <meshLine attach="geometry" vertices={vertices} />
+      <meshLineMaterial
+        attach="material"
+        ref={mat}
+        transparent={true}
+        depthTest={false}
+        sizeAttenuation={true}
+        lineWidth={0.01}
+        dashArray={0.5}
+        dashRatio={0.9}
+        opacity={0.75}
+      />
+    </mesh>
+  )
+}
+
+const Slashes = () => {
+  const lines = useMemo(() => {
+    return new Array(5).fill(0).map((_v, i) => {
+      const unit = 45
+      return [
+        new Vector3(
+          length * Math.cos((unit * i * Math.PI) / 180),
+          length * Math.sin((unit * i * Math.PI) / 180),
+          0,
+        ),
+        new Vector3(0, 0, 0),
+      ]
+    })
+  }, [])
+  return (
+    <>
+      {lines.map((l) => {
+        return <Slash vertices={l} />
+      })}
+    </>
+  )
+}
+
 const RainPage = () => {
   return (
     <Canvas
@@ -43,9 +96,10 @@ const RainPage = () => {
       <Controls enableDamping={true} rotateSpeed={0.3} dampingFactor={1} />
       <fog attach="fog" args={[0xffffff, 100, 100]} />
       <Rain />
-      <Suspense fallback="loading...">
+      {/* <Suspense fallback="loading...">
         <Text color="#f1f0ed">雨</Text>
-      </Suspense>
+      </Suspense> */}
+      <Slashes />
     </Canvas>
   )
 }
