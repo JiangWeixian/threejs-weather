6bee9d6aa344d36ec1ca4fb0b34b1a0c7ba2fd61
jiangweixian <jiangweixian1994@gmail.com>
2020-05-28 11:58:51 +0800

    🔥 remove components all

--- components/index.tsx
+++
@@ -1,9 +0,0 @@
-import Rain from './rain'
-import Sun from './sun'
-import StarRings from './star-rings'
-import Snow from './snow'
-import Meteors from './meteors'
-import Cloudy from './cloudy'
-import Wind from './wind'
-
-export { Rain, Sun, StarRings, Snow, Meteors, Cloudy, Wind }
--- components/utils/constants.ts
+++
@@ -1,1 +0,0 @@
-export const DIRS = [1, -1]
