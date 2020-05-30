f9c359c41634c494d38c0592f5b55ca2a6478b36
jiangweixian <jiangweixian1994@gmail.com>
2020-05-30 10:53:10 +0800

    🔥 (cloudy): usestate useless

--- components/cloudy/index.tsx
+++ components/cloudy/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 
 import { useClouds, UseCloudsProps, Cloud } from './use-cloud'
 import { useFrame } from 'react-three-fiber'
