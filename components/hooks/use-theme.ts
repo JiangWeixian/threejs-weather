1bb4fc18a36cc4b1f7ff8d1b1b731998962d6f25
jiangWeixian <jiangweixian1994@gmail.com>
2021-08-24 23:17:24 +0800

    :sparkles:  usetheme

---
+++ components/hooks/use-theme.ts
@@ -0,0 +1,96 @@
+import { useCallback, CSSProperties, useMemo } from 'react'
+
+export type UseThemeProps = {
+  type:
+    | 'cloudy'
+    | 'fog'
+    | 'haze'
+    | 'meteors'
+    | 'partly-cloudy'
+    | 'rain'
+    | 'snow'
+    | 'sun'
+    | 'star-rings'
+    | 'sun'
+  mode: 'day' | 'night'
+}
+
+const dayTheme: Record<UseThemeProps['type'], {
+  style: CSSProperties,
+  shadows?: boolean
+}> = {
+  'cloudy': {
+    style: {
+      backgroundColor: '#3C4245'
+    }
+  },
+  fog: {
+    style: {
+      backgroundColor: '#000',
+      shadows: true
+    }
+  },
+  haze: {
+    style: {
+      backgroundColor: '#A2915E',
+      shadows: true
+    }
+  },
+  meteors: {
+    style: {
+      backgroundColor: '#0F203B'
+    }
+  },
+  meteors: {
+    style: {
+      backgroundColor: '#0F203B'
+    }
+  },
+  "partly-cloudy": {
+    style: {
+      backgroundColor: '#1677b3'
+    }
+  },
+  "rain": {
+    style: {
+      backgroundColor: '#1677b3'
+    }
+  },
+  "snow": {
+    style: {
+      backgroundColor: '#1677b3'
+    }
+  },
+  "star-rings": {
+    style: {
+      backgroundColor: '#0F203B'
+    }
+  },
+  "sun": {
+    style: {
+      backgroundColor: '#faf4e8'
+    }
+  },
+}
+
+const nightTheme = dayTheme
+
+const theme: Record<UseThemeProps['mode'], typeof dayTheme> = {
+  day: dayTheme,
+  night: nightTheme,
+}
+
+export const useTheme = (props: UseThemeProps) => {
+  const config = useMemo(() => {
+    return theme[props.mode][props.type]
+  }, [props.mode, props.type])
+  const bind = useCallback(() => {
+    return {
+      ...config,
+      dpr: window.devicePixelRatio,
+    }
+  }, [config])
+  return {
+    bind,
+  }
+}
--- components/index.tsx
+++ components/index.tsx
@@ -8,5 +8,6 @@ import RainRing from './rain-ring'
 import PartlyCloudy from './partly-cloudy'
 import Fog from './fog'
 import Haze from './haze'
+import { useTheme } from './hooks/use-theme'
 
-export { Rain, Sun, StarRings, Snow, Meteors, Cloudy, RainRing, PartlyCloudy, Fog, Haze }
+export { Rain, Sun, StarRings, Snow, Meteors, Cloudy, RainRing, PartlyCloudy, Fog, Haze, useTheme }
