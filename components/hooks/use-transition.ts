b9e1511b66af8ba26f3349a442fd9c8c1d03e93f
jiangWeixian <jiangweixian1994@gmail.com>
2021-09-04 13:54:33 +0800

    :bug:  typo

--- components/hooks/use-transition.ts
+++ components/hooks/use-transition.ts
@@ -1,15 +1,16 @@
-import { useTransition as useSpringTransition, UseTransitionProps } from '@react-spring/core'
+import { useTransition as useSpringTransition } from '@react-spring/core'
 
 type TransitionProps = {
   location: any
+  delay?: number
 }
 
-export const useTransition = (props: TransitionProps & UseTransitionProps) => {
+export const useTransition = (props: TransitionProps) => {
   const transition = useSpringTransition(props.location, {
     from: { opacity: 0, scale: [0, 0, 0] },
     enter: { opacity: 1, scale: [1, 1, 1] },
     leave: { opacity: 0, scale: [0, 0, 0] },
-    ...props,
+    delay: props.delay,
   })
   return {
     transition,
